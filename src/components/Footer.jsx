6e9e9ff849d187213f32a11b87612c7043f5e9e9
Asif Ullah Sazzad <asifullahsazzad0@gmail.com>
2026-03-06 15:33:05 +0600

    Initial commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,40 @@
+import React from 'react';
+
+const Footer = ({title, subtitle}) => {
+    return (
+        <div className='container mx-auto'>
+
+            <div className='text-center space-y-3 pb-5'>
+                <h1 className='text-3xl font-bold'>{title}</h1>
+                <p className='text-base text-[rgba(9,8,15,0.6)]'>{subtitle}</p>
+            </div>
+
+            <hr className='text-[rgba(9,8,15,0.1)]'/>
+
+    <footer className="footer sm:footer-horizontal text-base-content justify-evenly pb-10 pt-5">
+    <nav className='place-items-center'>
+        <h6 className="font-bold text-lg">Services</h6>
+        <a className="link link-hover text-[rgba(9,8,15,0.6)]">Product Support</a>
+        <a className="link link-hover text-[rgba(9,8,15,0.6)]">Order Tracking</a>
+        <a className="link link-hover text-[rgba(9,8,15,0.6)]">Shipping & Delivery</a>
+        <a className="link link-hover text-[rgba(9,8,15,0.6)]">Returns</a>
+    </nav>
+    <nav className='place-items-center'>
+        <h6 className="font-bold text-lg">Company</h6>
+        <a className="link link-hover text-[rgba(9,8,15,0.6)]">About us</a>
+        <a className="link link-hover text-[rgba(9,8,15,0.6)]">Careers</a>
+        <a className="link link-hover text-[rgba(9,8,15,0.6)]">Contact</a>
+    </nav>
+    <nav className='place-items-center'>
+        <h6 className="font-bold text-lg">Legal</h6>
+        <a className="link link-hover text-[rgba(9,8,15,0.6)]">Terms of use</a>
+        <a className="link link-hover text-[rgba(9,8,15,0.6)]">Privacy policy</a>
+        <a className="link link-hover text-[rgba(9,8,15,0.6)]">Cookie policy</a>
+    </nav>
+    </footer>
+
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/layouts/MainLayouts.jsx
@@ -0,0 +1,26 @@
+import Navbar from '../components/Navbar';
+import Footer from '../components/Footer';
+import { Outlet } from 'react-router-dom';
+
+const MainLayouts = () => {
+    return (
+        <div>
+            {/* Navbar */}
+            <div className='container mx-auto'>
+                <Navbar></Navbar>
+            </div>
+            {/* Components */}
+            <div className='min-h-[calc(100vh-357px)]'>
+                <Outlet></Outlet>
+            </div>
+
+            {/* Footer */}
+            <Footer
+            title={'Gadget Heaven'}
+            subtitle={'Leading the way in cutting-edge technology and innovation.'}
+            ></Footer>
+        </div>
+    );
+};
+
+export default MainLayouts;
